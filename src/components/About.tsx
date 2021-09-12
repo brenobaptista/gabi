66c8a7b9275faa8051367dcf3aec7446b678b7e0
Breno Baptista <brenobaptista@protonmail.com>
2021-09-12 12:35:32 -0300

    Added About section

---
+++ src/components/About.tsx
@@ -0,0 +1,34 @@
+const About = (): JSX.Element => (
+  <section>
+    <div className='container mx-auto'>
+      <div className='lg:flex items-center justify-around px-8 pt-16 pb-24'>
+        <img
+          src='/images/gabi.png'
+          alt='Gabriela da Silva'
+          className='h-60 w-60 rounded-full bg-purple-300 dark:bg-gray-800 mx-auto lg:mx-0 mb-16 lg:mb-0 animate-eclipse'
+          width='240'
+          height='240'
+        />
+        <div>
+          <h1 className='text-3xl font-bold tracking-wide pb-4'>ABOUT ME</h1>
+          <p className='max-w-xl leading-loose pb-4 text-base lg:text-lg text-justify'>
+            I’m Gabriela, a mechanical engineer from Brazil. I’m passionate
+            about product design. Currently, I work as a freelancer CAD
+            designer. I have experience in complex mechanical design projects
+            and 3D printing prototypes.
+          </p>
+          <p className='max-w-xl leading-loose pb-4 text-base lg:text-lg text-justify'>
+            My services consist mainly of 3D modeling, 2D technical drawing and
+            rendering. I usually use tools like SolidWorks, Autodesk Fusion 360
+            and AutoCad in my projects. I’m constantly tinkering with new
+            technologies and learning new skills. I believe in CAD designing as
+            a means to positively impact businesses in making vital decisions
+            about major projects.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default About
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import About from '../components/About'
 import Hero from '../components/Hero'
 
 const Home = (): JSX.Element => (
   <div className='text-gray-800 dark:text-gray-200 bg-purple-200 dark:bg-gray-900 font-sans antialiased'>
     <Hero />
+    <About />
   </div>
 )
 
