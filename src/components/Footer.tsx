86c753f8179d93fd6a96f04ede3518f7ac3642e9
Breno Baptista <brenobaptista@protonmail.com>
2021-09-12 13:31:14 -0300

    Fixed footer for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,20 +2,23 @@ import Heart from './icons/Heart'
 
 const Footer = (): JSX.Element => (
   <footer className='bg-purple-300 dark:bg-gray-800'>
-    <div className='text-center py-2'>
-      © {new Date().getFullYear()} Gabriela da Silva. Made with
-      <div className='inline-flex'>
-        <Heart width={26} height={13} />
-      </div>
-      by{' '}
-      <a
-        href='https://brenobaptista.vercel.app/'
-        className='text-purple-600 dark:text-gray-400 hover:opacity-75'
-        target='_blank'
-        rel='noreferrer noopener'
-      >
-        Breno Baptista
-      </a>
+    <div className='text-center py-2 flex flex-col sm:block'>
+      <span>© {new Date().getFullYear()} Gabriela da Silva. </span>
+      <span>
+        Made with
+        <div className='inline-flex'>
+          <Heart width={26} height={13} />
+        </div>
+        by{' '}
+        <a
+          href='https://brenobaptista.vercel.app/'
+          className='text-purple-900 dark:text-gray-400 hover:opacity-75'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          Breno Baptista
+        </a>
+      </span>
     </div>
   </footer>
 )
