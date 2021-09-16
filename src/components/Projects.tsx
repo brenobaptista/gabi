a201d1ad880c11703dc850fbfdcb24f59fbb551b
Breno Baptista <brenobaptista@protonmail.com>
2021-09-16 13:06:39 -0300

    Added projects section

---
+++ src/components/Projects.tsx
@@ -0,0 +1,135 @@
+const Projects = (): JSX.Element => (
+  <section className='bg-purple-300 dark:bg-gray-800'>
+    <div className='container mx-auto px-8 pt-32'>
+      <div className='flex flex-wrap'>
+        <div className='w-full lg:w-1/3 pb-32'>
+          <div className='group flex justify-center items-center'>
+            <div className='group-hover:opacity-25 transition duration-300 ease-in-out'>
+              <img src='/images/projects/conveyor.png' alt='Conveyor belt' />
+            </div>
+            <div className='absolute opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out'>
+              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+                Conveyor belt
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full lg:w-1/3 pb-32'>
+          <div className='group flex justify-center items-center'>
+            <div className='group-hover:opacity-25 transition duration-300 ease-in-out'>
+              <img src='/images/projects/winch.png' alt='Hydraulic winch' />
+            </div>
+            <div className='absolute opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out'>
+              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+                Hydraulic winch
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full lg:w-1/3 pb-32'>
+          <div className='group flex justify-center items-center'>
+            <div className='group-hover:opacity-25 transition duration-300 ease-in-out'>
+              <img src='/images/projects/impeller.png' alt='Impeller' />
+            </div>
+            <div className='absolute opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out'>
+              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+                Impeller
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full lg:w-1/3 pb-32'>
+          <div className='group flex justify-center items-center'>
+            <div className='group-hover:opacity-25 transition duration-300 ease-in-out'>
+              <img
+                src='/images/projects/container.png'
+                alt='Plastic container'
+              />
+            </div>
+            <div className='absolute opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out'>
+              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+                Plastic container
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full lg:w-1/3 pb-32'>
+          <div className='group flex justify-center items-center'>
+            <div className='group-hover:opacity-25 transition duration-300 ease-in-out'>
+              <img
+                src='/images/projects/alignment.png'
+                alt='Mechanical alignment'
+              />
+            </div>
+            <div className='absolute opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out'>
+              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+                Mechanical alignment
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full lg:w-1/3 pb-32'>
+          <div className='group flex justify-center items-center'>
+            <div className='group-hover:opacity-25 transition duration-300 ease-in-out'>
+              <img src='/images/projects/support.png' alt='Laptop support' />
+            </div>
+            <div className='absolute opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out'>
+              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+                Laptop support
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full lg:w-1/3 pb-32'>
+          <div className='group flex justify-center items-center'>
+            <div className='group-hover:opacity-25 transition duration-300 ease-in-out'>
+              <img src='/images/projects/table.png' alt='Wooden table' />
+            </div>
+            <div className='absolute opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out'>
+              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+                Wooden table
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full lg:w-1/3 pb-32'>
+          <div className='group flex justify-center items-center'>
+            <div className='group-hover:opacity-25 transition duration-300 ease-in-out'>
+              <img src='/images/projects/flowerpot.png' alt='Flowerpot' />
+            </div>
+            <div className='absolute opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out'>
+              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+                Flowerpot
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full lg:w-1/3 pb-32'>
+          <div className='group flex justify-center items-center'>
+            <div className='group-hover:opacity-25 transition duration-300 ease-in-out'>
+              <img
+                src='/images/projects/component.png'
+                alt='Custom component'
+              />
+            </div>
+            <div className='absolute opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out'>
+              <span className='text-3xl font-bold tracking-wider leading-relaxed'>
+                Custom component
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default Projects
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import About from '../components/About'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
+import Projects from '../components/Projects'
 
 const Home = (): JSX.Element => (
   <div className='text-gray-800 dark:text-gray-200 bg-purple-200 dark:bg-gray-900 font-sans antialiased'>
     <Hero />
     <About />
+    <Projects />
     <Footer />
   </div>
 )
